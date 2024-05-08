4460c9da5bbd630da0a30c90e1e53137063eb80e
Panchal Aayush <86279304+panchalaayush1611@users.noreply.github.com>
2024-05-08 15:35:03 +0530

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,4 @@
+const li = document.getElementById("change_theme");
+const theme = document.getElementById('icon1').addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
